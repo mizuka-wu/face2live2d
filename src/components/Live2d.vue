79b4c0316373fd9664ffb8f9a7890e9fc4533518
水华 <mizuka.wu@outlook.com>
2021-07-26 23:25:35 +0800

    feat: register

--- src/components/Live2d.vue
+++ src/components/Live2d.vue
@@ -1,11 +1,12 @@
 <template>
-  <canvas ref="container" />
+  <canvas class="width: 100%" ref="container" style="width: 100%;height: 100%" />
 </template>
 
 <script lang="ts" setup>
 import { onMounted, defineProps, ref, Ref } from 'vue'
 import * as PIXI from 'pixi.js'
 import { Live2DModel } from 'pixi-live2d-display/dist/cubism4.min.js'
+Live2DModel.registerTicker(PIXI.Ticker)
 
 const props = defineProps({
   path: {
@@ -21,13 +22,37 @@ const container: Ref<HTMLCanvasElement | null> = ref(null)
 const app: Ref<PIXI.Application | null> = ref(null)
 const model: Ref<Live2DModel | null> = ref(null)
 onMounted(async () => {
-  app.value = new PIXI.Application({
-    view: container.value as HTMLCanvasElement,
-    autoStart: true
-  })
   model.value = await Live2DModel.from(props.path)
+  const { width, height } = model.value
+
+  app.value = new PIXI.Application({
+    transparent: true,
+    view: container.value as HTMLCanvasElement,
+    autoStart: true,
+    width,
+    height
+  })
   app.value.stage.addChild(model.value)
+
+  model.value.rotation = Math.PI
+  model.value.skew.x = Math.PI
+  model.value.x = width / 2
+  model.value.y = height / 2
+
+  model.value.scale.set(1, 1)
+  model.value.anchor.set(0.5, 0.5)
+
+  // 交互
+  model.value.on('hit', (hitAreas: any) => {
+    if (hitAreas.includes('body')) {
+      model.value.motion('tap_body')
+    }
+  })
 })
+
+/**
+ * 大小调整
+ */
 
 </script>
 
