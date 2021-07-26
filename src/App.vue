d4566271b4d00c08aaad45de410bec21aac13b91
水华 <mizuka.wu@outlook.com>
2021-07-26 22:55:28 +0800

    feat: App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  <Live2d path="./live2d/kueshishui.model3.json"></Live2d>
+  <div id="app">
+    <Live2d :path="path"></Live2d>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
 import Live2d from './components/Live2d.vue'
+const path = '/live2d/kueshishui.model3.json'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-    Live2d
-  }
-})
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
