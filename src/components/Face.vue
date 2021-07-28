f4cb96842a0fcd01b22b31c169172437c770fec5
水华 <mizuka.wu@outlook.com>
2021-07-28 22:25:17 +0800

    feat: 加载face-api

---
+++ src/components/Face.vue
@@ -0,0 +1,17 @@
+<template>
+    <canvas ref="face" />
+</template>
+<script lang="ts" setup>
+import { onBeforeMount } from 'vue'
+import * as faceapi from 'face-api.js'
+
+onBeforeMount(async () => {
+  try {
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+    console.log(faceapi)
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// navigator.getUserMedia({
+//     video: true
+// }, (media) => {
+//     console.log(media)
+// }, () => {
+//     console.log('摄像头失败')
+// })
 
 createApp(App).mount('#app')
